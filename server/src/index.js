c4aff11eec8f396d4d14a534cc6d2a322973547e
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-04-25 14:48:26 +0200

    Connect to MongoDB instance

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
+const { mongoUri } = require('../config')
 const authRoutes = require('./routes/authRoutes');
 
 
@@ -9,8 +11,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
 
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
+
 app.get('/', (req, res) => {
-  res.send(200);
+  res.send(`Your email: ${req.user.email}`);
 });
 
 const port = process.env.PORT || 3000;
