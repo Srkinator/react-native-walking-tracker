a092d47dcf6897e1fb478c0fe805847933779749
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-04-27 19:11:20 +0200

    Implement nested navigators, authFlow and mainFlow

--- react-native-running-tracker-app/App.js
+++ react-native-running-tracker-app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
@@ -11,38 +12,50 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
 const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const TrackListFlow = props => <Stack.Navigator>
+  <Stack.Screen name="TrackList" component={TrackListScreen} />
+  <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
+</Stack.Navigator>
 
 
 
 export default () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signup">
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-        />
-        <Stack.Screen
-          name="TrackDetails"
-          component={TrackDetailScreen}
-        />
-        <Stack.Screen
-          name="TrackList"
-          component={TrackListScreen}
-        />
-        <Stack.Screen
-          name="TrackCreate"
-          component={TrackCreateScreen}
-        />
-        <Stack.Screen
-          name="Account"
-          component={AccountScreen}
-        />
-        <Stack.Screen
-          name="Signin"
-          component={SigninScreen}
-        />
-      </Stack.Navigator>
+      {false // isLoggedIn
+        ? <React.Fragment>
+          <BottomTab.Navigator>
+            <BottomTab.Screen
+              name="TrackListFlow"
+              component={TrackListFlow}
+              options={() => ({
+                title: 'Tracks',
+                tabBarIcon: _ => <FontAwesome name="th-list" size={20} />
+              })}
+            />
+            <BottomTab.Screen
+              name="TrackCreate"
+              component={TrackCreateScreen}
+            />
+            <BottomTab.Screen
+              name="Account"
+              component={AccountScreen}
+            />
+          </BottomTab.Navigator>
+        </React.Fragment>
+        : <Stack.Navigator initialRouteName="Signup">
+          <Stack.Screen
+            name="Signup"
+            component={SignupScreen}
+          />
+          <Stack.Screen
+            name="Signin"
+            component={SigninScreen}
+          />
+        </Stack.Navigator>
+      }
     </NavigationContainer>
   );
 };
