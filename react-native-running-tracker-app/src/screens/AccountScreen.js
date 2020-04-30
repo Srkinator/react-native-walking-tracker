c1ab08510797ec3956bac4b007b6ff8ba47ed32d
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-04-30 21:50:45 +0200

    Implement signout action in the AccountScreen

--- react-native-running-tracker-app/src/screens/AccountScreen.js
+++ react-native-running-tracker-app/src/screens/AccountScreen.js
@@ -1,15 +1,27 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Spacer from '../components/Spacer';
 import { AuthContext } from '../context/AuthContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
-  const { signout } = useContext(AuthContext)
+  const { signout } = useContext(AuthContext);
 
   return (
-    <View>
+    <SafeAreaView forceInset={{ top: 'always' }}>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
-    </View>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
 };
 
 const styles = StyleSheet.create({});
