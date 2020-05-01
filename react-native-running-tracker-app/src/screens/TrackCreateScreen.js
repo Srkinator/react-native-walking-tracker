d28968447c880f227a8d0627a707a5c25e454b99
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-05-01 20:04:09 +0200

    Ask user to grant permission to access location and read current position

--- react-native-running-tracker-app/src/screens/TrackCreateScreen.js
+++ react-native-running-tracker-app/src/screens/TrackCreateScreen.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
+
 
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [errorMsg, setErrorMsg] = useState('')
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [])
+
+  if (!location) {
+    return null
+  }
+
   return (
     <SafeAreaView>
       <View>
-        <Map />
+        <Map
+          initialRegion={{
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude,
+          }}
+        />
       </View>
     </SafeAreaView>
   );
