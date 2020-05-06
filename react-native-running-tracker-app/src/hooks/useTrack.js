67ecae0bf69ebad01a1649c2da831cc9050c3bc6
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-05-06 21:28:10 +0200

    Refactor useTrack hook, change actions names and add loading state

--- react-native-running-tracker-app/src/hooks/useTrack.js
+++ react-native-running-tracker-app/src/hooks/useTrack.js
@@ -3,10 +3,19 @@ import { useReducer } from 'react'
 import axios from '../api/axios';
 
 
+const initialState = {
+  tracks: [],
+  loading: false
+}
+
 const trackReducer = (state, action) => {
   switch (action.type) {
-    case 'fetch_tracks':
-      return action.payload;
+    case 'fetch_tracks_request':
+      return { ...state, loading: true };
+    case 'fetch_tracks_success':
+      return { ...state, loading: false, tracks: action.payload };
+    case 'fetch_tracks_error':
+      return { ...state, loading: false, err: action.payload };
     default:
       return state;
   }
@@ -14,11 +23,16 @@ const trackReducer = (state, action) => {
 
 
 const useTrack = () => {
-  const [state, dispatch] = useReducer(trackReducer, {})
+  const [state, dispatch] = useReducer(trackReducer, initialState)
 
   const fetchTracks = async () => {
-    const response = await axios.get('/tracks');
-    dispatch({ type: 'fetch_tracks', payload: response.data });
+    dispatch({ type: 'fetch_tracks_request' })
+    try {
+      const response = await axios.get('/tracks');
+      dispatch({ type: 'fetch_tracks_success', payload: response.data });
+    } catch (e) {
+      dispatch({ type: 'fetch_tracks_error' })
+    }
   };
 
   const createTrack = async (name, locations) => {
