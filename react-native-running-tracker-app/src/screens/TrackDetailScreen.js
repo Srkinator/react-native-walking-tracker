ff473f0df326b99c74803636c98adb5006db0498
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-05-07 21:23:54 +0200

    Display recorded track path using the MapView component in Track Details Screen

--- react-native-running-tracker-app/src/screens/TrackDetailScreen.js
+++ react-native-running-tracker-app/src/screens/TrackDetailScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 import { useTrackContext } from '../context/TrackContext';
 
@@ -9,10 +10,21 @@ const TrackDetailScreen = ({ navigation, route }) => {
   const id = route.params.id;
 
   const track = tracks.find(t => t._id === id);
+  const initialCoords = track.locations[0].coords;
 
   return (
     <>
       <Text style={{ fontSize: 48 }}>{track.name}</Text>
+      <MapView
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords
+        }}
+        style={styles.map}
+      >
+        <Polyline coordinates={track.locations.map(loc => loc.coords)} />
+      </MapView>
     </>
   );
 };
